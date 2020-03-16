2a9ceea1cb4fca241bb11ef5c8b972c97bfc8b44
ajaymishra0901 <54370352+ajaymishra0901@users.noreply.github.com>
2020-03-16 14:01:44 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,11 +1,15 @@
+#importing smtplib
 import smtplib
 
+#write content
 content = 'hello, sending mail from python code'
 mail = smtplib.SMTP('smtp.gmail.com', 587)
 mail.ehlo()
 mail.starttls()
 
+#for login
 mail.login('@gmail.com', password)
 mail.sendmail('@gmail.com', '@gmail.com', content)
 
+#closing mail
 mail.close()
